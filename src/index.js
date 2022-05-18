442c7219cb28c598242924ad0282a8139ab7835d
Marco <marcovelardejr@gmail.com>
2022-05-17 20:44:51 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let taskForm = document.getElementById('create-task-form');
+  taskForm.addEventListener('submit', funSubmit)
+
 });
+
+function funSubmit(e) {
+  e.preventDefault();
+
+  let taskList = document.getElementById('tasks');
+  let task = document.createElement('li');
+  task.textContent = e.target['new-task-description'].value
+  taskList.append(task);
+
+  let btn = document.createElement('button');
+  btn.textContent = 'x'
+  btn.addEventListener('click', handleDelete)
+  task.append(btn);
+  
+  e.target.reset();
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
